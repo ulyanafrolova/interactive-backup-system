60159fb0de7ce44b4b2431ec4b83dab70d9164c7
Ulyana <6291709@gmail.com>
2026-07-09 14:55:39 +0300

    Uploaded project

---
+++ parser.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Split the input command string into an array of strings
+char** split_command(char* input, int* argc);
